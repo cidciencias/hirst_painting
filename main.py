98885b4b1841f636ccb20c446035c3199fc4452d
Luis Cid <alexcid2002@gmail.com>
2024-05-29 20:21:30 +0100

    hirst_painting V2.0.0
    Circle draw logic implemented.
    Program prints 10 circles, then comes back to print another 10 and so on.
    It stops when the number of dots equals 100

--- main.py
+++ main.py
@@ -1,11 +1,49 @@
 import colorgram
+import random
+import turtle as t
+
 
 # Retrieve colors from the image, and store them in rgb_colors[]
 # rgb_colors = []
 # colors = colorgram.extract('image.jpg', 30)
 # for color in colors:
-#     rgb_colors.append(color.rgb)
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r, g, b)
+#     rgb_colors.append(new_color)
 #
 # print(rgb_colors)
 
-# [Rgb(r=245, g=243, b=238), Rgb(r=246, g=242, b=244), Rgb(r=202, g=164, b=110), Rgb(r=240, g=245, b=241), Rgb(r=236, g=239, b=243), Rgb(r=149, g=75, b=50), Rgb(r=222, g=201, b=136), Rgb(r=53, g=93, b=123), Rgb(r=170, g=154, b=41), Rgb(r=138, g=31, b=20), Rgb(r=134, g=163, b=184), Rgb(r=197, g=92, b=73), Rgb(r=47, g=121, b=86), Rgb(r=73, g=43, b=35), Rgb(r=145, g=178, b=149), Rgb(r=14, g=98, b=70), Rgb(r=232, g=176, b=165), Rgb(r=160, g=142, b=158), Rgb(r=54, g=45, b=50), Rgb(r=101, g=75, b=77), Rgb(r=183, g=205, b=171), Rgb(r=36, g=60, b=74), Rgb(r=19, g=86, b=89), Rgb(r=82, g=148, b=129), Rgb(r=147, g=17, b=19), Rgb(r=27, g=68, b=102), Rgb(r=12, g=70, b=64), Rgb(r=107, g=127, b=153), Rgb(r=176, g=192, b=208), Rgb(r=168, g=99, b=102)]
+colors = [(245, 243, 238), (246, 242, 244), (202, 164, 110), (240, 245, 241), (236, 239, 243), (149, 75, 50),
+          (222, 201, 136), (53, 93, 123), (170, 154, 41), (138, 31, 20), (134, 163, 184), (197, 92, 73), (47, 121, 86),
+          (73, 43, 35), (145, 178, 149), (14, 98, 70), (232, 176, 165), (160, 142, 158), (54, 45, 50), (101, 75, 77),
+          (183, 205, 171), (36, 60, 74), (19, 86, 89), (82, 148, 129), (147, 17, 19), (27, 68, 102), (12, 70, 64),
+          (107, 127, 153), (176, 192, 208), (168, 99, 102)]
+
+t.colormode(255)
+tim = t.Turtle()
+tim.speed("fastest")
+tim.penup()
+tim.hideturtle()
+tim.setheading(225)
+tim.forward(300)
+tim.setheading(0)
+num_of_dots = 100
+
+for dot_count in range(1, num_of_dots + 1):
+    tim.dot(20, random.choice(colors))
+    tim.forward(50)
+
+    if dot_count % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
+
+
+
+
+screen = t.Screen()
+screen.exitonclick()
